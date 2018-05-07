fc95d9c64f0e5622c5a2fa7390b033a4f7fb639f
santosh kumar <36450386+santoshkumarbhonagiri@users.noreply.github.com>
2018-05-07 12:10:57 +0530

    Update design.js

--- design.js
+++ design.js
@@ -1,36 +1,65 @@
-# PixelArtmaker
-function makeGrid(width, height) {
-	let pixelCanvas, grid;
-	pixelCanvas = $('#pixel_canvas');
-	if (pixelCanvas.children().length) {
-		pixelCanvas.empty();
-	}
-	for(let i = 0; i < height; i++) {
-		grid += "<tr>";
-		for (let j = 0; j < width; j++) {
-			grid += "<td></td>";
-		}
-		grid += "</tr>";
-	}
-	pixelCanvas.html(grid);
-	pixelCanvas.css("background-color", "#ffffff");
+let Down;
+
+$("input[type = 'submit']").click(function(e){
+  e.preventDefault();
+  makeGrid();
+})
+
+
+function makeGrid() {
+
+
+var h=$("#inputHeight").val();
+var w=$("#inputWeight").val();
+
+var table=$("#pixelCanvas");
+
+table.children().remove();
+
+if (h>100){
+  h=100
+}
+if (w>100){
+  w=100
+}
+
+for (var i = 0; i < h; i++) {
+  table.last().append("<tr> </tr>");
+  for (var j = 0; j < w; j++) {
+    table.children().last().append("<td></td>");
+  }
+}
 };
 
-$(document).ready(function(){
-	$('#submit-btn').click(function(evt) {
-		let height, width;
-		width = $('#input_width').val();
-		height = $('#input_height').val();
-		evt.preventDefault();
-		makeGrid(width, height);
-	});
 
-	$('#pixel_canvas').click(function(evt) {
-		let color = $('#colorPicker').val();
-		$(evt.target).css('background-color', color);
-	});
+
+$( 'table' ).on( 'click', 'td' , function() {
+    var colour = $('#colorPicker').val();
+    $(this).css( 'background', colour );
 });
 
+$(document).mousedown(function () {
+    Down = true;
+})
+$(document).mouseup(function () {
+    Down = false;
+});
 
+$('table').on('mouseover', 'td', function () {
+    if (Down) {
+      var colour = $('#colorPicker').val();
+        $(this).css( 'background', colour );
+    }
+});
 
+$('table').on('dblclick', 'td', function(){
+    $(this).css("background-color", "white");
+});
+ 
+ function resetGrid(){
+   $('#pixelCanvas').empty();
+ }
 
+function resetColour(){
+  $('td').css({"background-color": ""});
+}
